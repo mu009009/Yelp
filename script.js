8a34d6bc6388dae7e81fca874f3d56a06f28e84c
maaria91 <Maaria@Maarias-MacBook-Air.local>
2016-04-01 13:55:48 -0400

    New layout + piechart

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
-//Set up drawing environment with margin conventions
-var margin = {t:20,r:20,b:50,l:50};
+//---------Setting up the canvas --------------//
+var canvas0Width = document.getElementById('canvas-1').clientWidth,
+    canvas0Height = document.getElementById('canvas-1').clientHeight;
 
-BussinessDataLoad();
+var canvas3Width = document.getElementById('canvas-3').clientWidth,
+    canvas3Height = document.getElementById('canvas-3').clientHeight;
+
+var margin = {t: 10, r: 50, b: 15, l: 100};
+var plotWidth = canvas0Width - margin.l - margin.r,
+    plotHeight = canvas0Height - margin.t - margin.b;
+
+var canvasWidth = canvas3Width - margin.l - margin.r,
+    canvasHeight = canvas3Height - margin.t - [margin.b + 70];
+
 
 var CategoriesDataRecord = [null];
 var CategoriesNumberArrary = [null];
@@ -12,283 +22,98 @@ var GroupArrary = null;
 var KeyWord = null;
 var StatesName = null;
 
-function BussinessDataLoad()
-{
-	queue()
-	.defer(d3.csv,'Data/Categories-Yelp.csv',parseTwo)
-	.defer(d3.csv,'Data/yelp-business.csv',parse)
-	.await(dataLoaded);
-}
+KeyWord = "PA";
+BussinessDataLoad();
 
-function dataLoaded(err,Type,Bussiness)
-{
-	StatesName = GetStates(Bussiness);
-	console.log(StatesName);
-	KeyWord = "PA";
-	CategoriesDataRecord = TranslateData(Bussiness);
-	CategoriesNumberArrary = FilterData(Bussiness,CategoriesDataRecord)
-	TypeData = AdditionalChange(CategoriesDataRecord,CategoriesNumberArrary,Type);
-	GroupData = NestGroupData(TypeData);
-	console.log(GroupData);
-	GroupArrary = GroupDataArrary(GroupData);
-	DrawPieChart(GroupArrary);
-}
-
-function parse(rows)
-{	
-	return {
-		BusinessId : rows.business_id,
-		Categories : CategoriesName(rows.categories),
-		City : rows.city,
-		Address : rows.full_address,
-		OpenTime : rows.hours,
-		Latitude : +rows.latitude,
-		Longitude : +rows.longitude,
-		Name : rows.name,
-		Neighborhoods : rows.neighborhoods,
-		Ifopen : rows.open,
-		Review : +rows.review_count,
-		Stars : +rows.stars,
-		States : rows.state,
-		Type : rows.type
-	}
-}
-
-function parseTwo(rows)
-{
-	return {
-		BussinessCategory: rows.Category,
-		BussinessGroup: rows.Group
-	}
-}
-
-function CategoriesName(Business)
-{						
-	var NewString = Business.replace("[","");
-		NewString = NewString.replace("]","");
-		NewString = NewString.split(",");
-		return NewString;
-}
-
-function TranslateData(Business)
-{
-	var TemporyRecord = [null];
-	for(var i=0;i<Business.length;i++)
-		{
-			if(Business[i].States == KeyWord)
-				{
-					for(var j=0;j<Business[i].Categories.length;j++)
-						{
-								for(var z=0;z<TemporyRecord.length;z++)
-									{
-										if(TemporyRecord[z]==Business[i].Categories[j])
-											{
-												break;
-											}
-										else
-											{
-												if(z==(TemporyRecord.length-1))
-													{
-														TemporyRecord.push(Business[i].Categories[j])
-														break;
-													}
-											}
-									}
-						}					
-				}
-		}
-	TemporyRecord.shift();
-	return TemporyRecord;
-}
-
-function FilterData(Bussiness,CategoriesDataRecord)
-{	
-	var StatesBussiness = [null];
-	for(var i=0; i<Bussiness.length;i++)
-		{
-			if(Bussiness[i].States == KeyWord)
-				{
-					StatesBussiness.push(Bussiness[i]);
-				}
-		}
-	
-	StatesBussiness.shift();
-	
-	var croosFilterChange = crossfilter(StatesBussiness);
-	
-	var CategoryFilter = croosFilterChange.dimension(function(d)
-	{
-		return d.Categories;
-	})
-	
-	var TemporyDataRecord = [null];
-	
-	for(var i=0;i<CategoriesDataRecord.length;i++)
-		{
-			TotleNumberCategoriesForeach = CategoryFilter.filter(function(d) 
-			{
-				if (d.indexOf(CategoriesDataRecord[i]) < 0) 
-				{
-					return false;
-				}
-				return true
-			})
-			.top(Infinity);
-			TemporyDataRecord.push(TotleNumberCategoriesForeach.length);
-		}
-	
-	TemporyDataRecord.shift();
-	
-	return TemporyDataRecord;
-}
-
-function AdditionalChange(CategoriesData,CategoriesNumberData,Type)
-{
-	var TemporyDataRecord = [{Categories:null,ValueNumber:null,Group:null}];
-	for(var i=0;i<CategoriesData.length;i++)
-		{
-			for(var j=0;j<Type.length;j++)
-				{
-					var JudgeString = '"'+ CategoriesData[i] + '"';
-					if(Type[j].BussinessCategory == JudgeString)
-						{
-							TemporyDataRecord.push({Categories:CategoriesData[i],ValueNumber:CategoriesNumberData[i],Group:Type[j].BussinessGroup});
-							break;
-						}
-				}
-		}
-	
-	TemporyDataRecord.shift();
-	return TemporyDataRecord;
-}
-
-function NestGroupData(TypeData)
-{
-	var TemporyDataRecord = [{Group:null,Value:null}];
-	for(var i=0;i<TypeData.length;i++)
-		{
-			for(var j=0;j<TemporyDataRecord.length;j++)
-				{
-					if(TypeData[i].Group == TemporyDataRecord[j].Group)
-						{
-							TemporyDataRecord[j].Value = TemporyDataRecord[j].Value + TypeData[i].ValueNumber;
-							break;
-						}
-					else if(j==TemporyDataRecord.length-1)
-						{
-							TemporyDataRecord.push({Group:TypeData[i].Group,Value:TypeData[i].ValueNumber});
-							break;
-						}
-				}
-		}
-	
-	TemporyDataRecord.shift();
-	return TemporyDataRecord;
-}
-
-function GroupDataArrary(GroupData)
-{
-	var TemporyDataRecord = [null];
-	for(var i=0;i<GroupData.length;i++)
-		{
-			TemporyDataRecord.push(GroupData[i].Value);
-		}
-	
-	TemporyDataRecord.shift();
-	return TemporyDataRecord;
-}
-
-function GetStates(Business)
-{
-	var TemporyDataRecord = [null];
-	for (var i=0;i<Business.length;i++)
-		{
-			for(var j=0;j<TemporyDataRecord.length;j++)
-				{
-					if(TemporyDataRecord[j]==Business[i].States)
-						{
-							break;
-						}
-					else if(j==TemporyDataRecord.length-1)
-						{
-							TemporyDataRecord.push(Business[i].States);
-							break;
-						}
-				}
-		}
-	
-	TemporyDataRecord.shift();
-	return TemporyDataRecord;
-}
-
-
-function DrawPieChart(Business)
-{
-//	console.log(Business);
-	var BusinessData = Business
-	var WidthPx = null;
-	var HeightPX = null;
-	var PieSvg = d3.select("#container1")
-	.append('svg')
-	.attr('class','svg')
-	.attr('id','PIeChartSvg')
-	.style('width',function()
-	{
-		WidthPx = document.getElementById('container1').offsetWidth;
-		return WidthPx + 'px';
-	})
-	.style('height',function()
-	{
-		HeightPX = document.getElementById('container1').offsetHeight*0.5;
-		return HeightPX + 'px';
-	});
-	
-	var OutRadius = HeightPX/(2.5);
-	var InnerRadius = HeightPX/7;
-	
-	var Arc = d3.svg.arc()  
-    .innerRadius(InnerRadius)  
-    .outerRadius(OutRadius);  
-	
-	var Pie = d3.layout.pie();
-	
-	var Xposition = document.getElementById('container1').offsetWidth*0.5;
-	var Yposition = document.getElementById('PIeChartSvg').offsetHeight*0.2;
-	
-	 var color = d3.scale.category10();
-	
-	 var arcs = PieSvg.selectAll("g")  
-         .data(Pie(BusinessData))  
-         .enter()  
-         .append("g")  
-         .attr("transform","translate("+OutRadius+","+InnerRadius+")");
-	
-	 arcs.append("path")  
-            .attr("fill",function(d,i){  
-		 			var number = i%80;
-		 		return color(i);
-//                return "rgb(" + "(255," + "255," + (255*(i%80*0.1)).toString()+")" ;  
-            })  
-            .attr("d",function(d){  
-                return Arc(d);  
-            })
-			.attr("transform", function()
-			{
-				Xposition = document.getElementById('container1').offsetWidth*0.6;
-				Yposition = document.getElementById('PIeChartSvg').offsetHeight*0.35;
-				var CircleString = "translate(" + Xposition.toString() + "," + Yposition.toString() + ")";
-				return CircleString;
-			}); 
-	
-	 arcs.append("text")  
-            .attr("transform",function(d){
-		 		Xposition = document.getElementById('container1').offsetWidth*0.6;
-				Yposition = document.getElementById('PIeChartSvg').offsetHeight*0.35;
-                return "translate(" + (Arc.centroid(d)[0]+Xposition) + "," + (Arc.centroid(d)[1]+Yposition) + ")";  
-            })  
-            .attr("text-anchor","middle")  
-            .text(function(d){  
-                return d.value;  
-            });  
-}
-
+//-------------------------------------  HEAT MAP  ---------------------------------------//
+//maybe this is how we select it on canvas 2?
+//var heatMap = d3.select("#canvas-2")
+//    .append('svg')
+//    .attr('class','svg')
+//    .attr('id','heatMap');
+//
+//
+////-------------- VARIABLES-----------------//
+////var days = d3.max(dataset, function(d) { return d.day; })
+////	- d3.min(dataset, function(d) { return d.day; });
+////var hours = d3.max(dataset, function(d) { return d.hour; })
+////	- d3.min(dataset, function(d) { return d.hour; });
+////
+//var StarsMin = d3.min(rows, function(d) { return d.Stars; }),
+//    StarsMax = d3.max(rows, function(d) { return d.Stars; });
+//
+//var dotWidth = 1,
+//    dotHeight = 3,
+//    dotSpacing = 0.5;
+//
+////var margin = {top: 0, right: 25, bottom: 40, left: 25},
+////	width = (dotWidth * 2 + dotSpacing) * days,
+////	height = (dotHeight * 2 + dotSpacing) * hours;//200 - margin.top - margin.bottom;
+//
+//
+//var scaleX = d3.time.scale()
+//    .domain([new Date(2012, 0, 1), new Date(2012, 11, 31)])
+//    .range([0, width]);
+//
+//var scaleY = d3.scale.ordinal().domain(Name).rangePoints([0, canvasWidth]) //to pull the NAME from the DATA - name of business
+//
+////Define the color scale
+//var colorScale = d3.scale.linear()
+//    .domain([StarsMin, StarsMax])
+//    .range(["#c41200","#ce882c", "#f5f5f5"]);
+//
+//// Define X axis
+//var xAxis = d3.svg.axis()
+//    .scale(scaleX)
+//    .orient("bottom")
+//    .ticks(d3.time.months)
+//    .tickFormat(d3.time.format("%b"));
+//
+//
+//// Define Y axis
+//var yAxis = d3.svg.axis()
+//    .scale(scaleY)
+//    .orient("left")
+////.ticks(2);
+//
+//
+////
+////// SVG canvas
+////var svg = d3.select("#chart")
+////	.append("svg")
+////	.attr("width", width + margin.left + margin.right)
+////	.attr("height", height + margin.top + margin.bottom)
+////	.call(zoom)
+////	.append("g")
+////	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//// Clip path
+//svg.append("clipPath")
+//    .attr("id", "clip")
+//    .append("rect")
+//    .attr("width", width)
+//    .attr("height", height);
+//
+//// The Heatmap dots
+//svg.append("g")
+//    .attr("clip-path", "url(#clip)")
+//    .selectAll("ellipse")
+//    .data(dataset)
+//    .enter()
+//    .append("ellipse")
+//    .attr("cx", function(d) { return scaleX(d.Name); })
+//    .attr("cy", function(d) { return scaleY(d.Month); })
+//    .attr("rx", dotWidth)
+//    .attr("ry", dotHeight)
+//    .attr("fill", function(d) { return colorScale(d.Stars); });
+//
+////Create X axis
+//svg.append("g")
+//    .attr("class", "x axis")
+//    .attr("transform", "translate(0," + yScale(0) + ")")
+//    .call(xAxis)
+//
+////Create Y axis
+//svg.append("g")
+//    .attr("class", "y axis")
+//    .call(yAxis);
